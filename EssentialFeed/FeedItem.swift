888f385b9ebd9e05fd07816939237edef8150481
Aleniten <jaherrero85@gmail.com>
2023-10-04 14:46:36 -0300

    Create Essential Feed and FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jose Herrero on 2023-10-04.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
