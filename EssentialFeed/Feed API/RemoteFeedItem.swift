a502e9e0fb1d0aee9dc8017a00eca81049e70062
Aleniten <jaherrero85@gmail.com>
2023-10-12 19:30:28 -0300

    Move RemoteFeedItem and LocalFeedItem to its respective file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jose Herrero on 2023-10-12.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
