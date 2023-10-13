9d55086a47af719fd64a5416342260ba46d69f1c
Aleniten <jaherrero85@gmail.com>
2023-10-12 22:02:28 -0300

    Load command delivers cache images on less than seven days old cache

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+public enum RetrieveCachedResult {
+    case empty
+    case found([LocalFeedImage], Date)
+    case failure(Error)
+}
+
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (Error?) -> Void
+    typealias RetrievalCompletion = (RetrieveCachedResult) -> Void
   
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timeStamp: Date, completion: @escaping InsertionCompletion)
