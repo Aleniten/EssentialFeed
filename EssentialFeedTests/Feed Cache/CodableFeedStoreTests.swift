b2f91b00396dd79692623dd6a535a65e47d7edff
Aleniten <jaherrero85@gmail.com>
2023-10-16 16:38:23 -0300

    Deleting an already empty cache has no side-effects (cache stays empty)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -67,6 +67,10 @@ class CodableFeedStore {
             completion(error)
         }
     }
+    
+    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+            completion(nil)
+        }
 }
 
 final class CodableFeedStoreTests: XCTestCase {
@@ -157,6 +161,33 @@ final class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
         expect(sut, toRetrieve: .empty)
     }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+            let sut = makeSUT()
+            let exp = expectation(description: "Wait for cache deletion")
+
+            sut.deleteCachedFeed { deletionError in
+                XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+                exp.fulfill()
+            }
+            wait(for: [exp], timeout: 1.0)
+
+            expect(sut, toRetrieve: .empty)
+        }
+    
+//    func test_delete_emptiesPreviouslyInsertedCache() {
+//            let sut = makeSUT()
+//            insert(uniqueImageFeed().local, timestamp: Date(), to: sut)
+//
+//            let exp = expectation(description: "Wait for cache deletion")
+//            sut.deleteCachedFeed { deletionError in
+//                XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
+//                exp.fulfill()
+//            }
+//            wait(for: [exp], timeout: 1.0)
+//
+//            expect(sut, toRetrieve: .empty)
+//        }
 
     // MARK: Helpers
     
