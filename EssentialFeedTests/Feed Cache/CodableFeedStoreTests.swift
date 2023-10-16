2e8f953864e49bb74c06407154b7dbdf21566083
Aleniten <jaherrero85@gmail.com>
2023-10-16 14:35:41 -0300

    Retrieving after inserting to empty cache deliver instead values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,23 +9,56 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timeStamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        let decoder = JSONDecoder()
+        let decoded = try! decoder.decode(Cache.self, from: data)
+        completion(.found(decoded.feed, decoded.timeStamp))
+        
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timeStamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timeStamp: timeStamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
 final class CodableFeedStoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
 
     func test_retrieve_deliversEmptyOnEmptyCache(){
-        let  sut = CodableFeedStore()
+        let sut = CodableFeedStore()
         let exp = expectation(description: "Wait for cache retrieval")
         sut.retrieve { result in
             switch result {
             case .empty:
                 break
             default:
-                XCTFail("Should Deliver a n  empty cache, got  instead \(result)")
+                XCTFail("Should Deliver an empty cache, got instead \(result)")
             }
             exp.fulfill()
         }
@@ -34,7 +67,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache(){
-        let  sut = CodableFeedStore()
+        let sut = CodableFeedStore()
         let exp = expectation(description: "Wait for cache retrieval")
         sut.retrieve { firstResult in
             sut.retrieve { secondResult in
@@ -43,6 +76,29 @@ final class CodableFeedStoreTests: XCTestCase {
                     break
                 default:
                     XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult)")
+                }
+                exp.fulfill()
+            }
+           
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues(){
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed()
+        let timeStamp = Date()
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.insert(feed.local, timeStamp: timeStamp) { insertionError in
+            XCTAssertNil(insertionError)
+            sut.retrieve { retrieveResult in
+                switch retrieveResult {
+                case let .found(retrievedFeed, retrievedTimeStamp):
+                    XCTAssertEqual(retrievedFeed, feed.local)
+                    XCTAssertEqual(retrievedTimeStamp, timeStamp)
+                default:
+                    XCTFail("Expected found result with result \(feed) and \(timeStamp), got instead \(retrieveResult)")
                 }
                 exp.fulfill()
             }
