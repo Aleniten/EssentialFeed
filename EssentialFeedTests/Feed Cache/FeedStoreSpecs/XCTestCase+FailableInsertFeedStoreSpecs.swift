1fdd59229b0bde21ee23666bd82a5eca527db06e
Aleniten <jaherrero85@gmail.com>
2023-10-17 12:45:23 -0300

    Create helpers for every test protocol and move into his owns files

---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableInsertFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Jose Herrero on 2023-10-17.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
+    func assertThatInsertDeliversErrorOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert(uniqueImageFeed().local, timestamp: Date(), to: sut)
+
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
+    }
+
+    func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        insert(uniqueImageFeed().local, timestamp: Date(), to: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
