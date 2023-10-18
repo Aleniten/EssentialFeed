5ea07fe5440568673debd88f0335bca126af384c
Aleniten <jaherrero85@gmail.com>
2023-10-18 10:19:04 -0300

    add integration tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Jose Herrero on 2023-10-18.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    func test_load_deliversNoItemsOnEmptyCache() {
+            
+    }
+
+}
