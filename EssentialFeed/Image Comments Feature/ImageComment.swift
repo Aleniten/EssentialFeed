442a3b1cd7446b004c2cddd328f1ba1bd88cbdd0
Aleniten <jaherrero85@gmail.com>
2023-11-02 11:10:48 -0300

    Add ImageComment data model

---
+++ EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Jose Herrero on 2023-11-02.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
