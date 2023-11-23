05a3d3ddbe5a1b5b23abf58c62bfa3c1bc89a3a3
Aleniten <jaherrero85@gmail.com>
2023-11-23 14:54:50 -0300

    Remove Unusued FeedImagePresenter Logic

--- EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -5,12 +5,9 @@
 //  Created by Jose Herrero on 2023-10-26.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
     
     public var hasLocation: Bool {
         return location != nil
--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -153,7 +153,7 @@ extension UIViewController {
 private extension FeedViewController {
     func display(_ stubs: [ImageStub]) {
         let cells: [FeedImageCellController] = stubs.map { stub in
-            let cellController = FeedImageCellController(delegate: stub)
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
             stub.controller = cellController
             return cellController
         }
@@ -217,22 +217,27 @@ private final class SnapshotWindow: UIWindow {
 
 
 private class ImageStub: FeedImageCellControllerDelegate {
-    let viewModel: FeedImageViewModel<UIImage>
+    let viewModel: FeedImageViewModel
+    let image: UIImage?
     weak var controller: FeedImageCellController?
     
     init(description: String?, location: String?, image: UIImage?) {
         viewModel = FeedImageViewModel(
             description: description,
-            location: location,
-            image:image,
-            isLoading: false,
-            shouldRetry: image == nil
+            location: location
         )
+        self.image = image
     }
     
     
     func didRequestImage() {
-        controller?.display(viewModel)
+        controller?.display(ResourceLoadingViewModel(isLoading: false))
+        if let image {
+            controller?.display(image)
+            controller?.display(ResourceErrorViewModel(errorMessage: .none))
+        } else {
+            controller?.display(ResourceErrorViewModel(errorMessage: "any"))
+        }
     }
     
     func didCancelImageRequest() {
